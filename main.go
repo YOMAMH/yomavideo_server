7623cc4b35eacd93e679c7721ed154c97c3f2afa
YOMAMH <741834601@qq.com>
2017-06-02 18:21:49 +0800

    init

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	_ "yomavideo_server/routers"
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/plugins/cors"
+)
+
+func main() {
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{"Origin"},
+		ExposeHeaders: []string{"Content-Length"},
+		AllowCredentials: true,    }))
+	beego.Run()
+}
+
+
